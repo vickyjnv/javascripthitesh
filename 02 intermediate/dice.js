42047e0db02d104c342655c5ea0a3882525d449a
Vicky <Kumar>
2020-06-23 02:02:01 +0530

    practice

---
+++ 02 intermediate/dice.js
@@ -0,0 +1,13 @@
+// let numbetOne = 22
+// let numberTwo = 7
+
+// result = 22 / 7
+// console.log(result.toFixed(2))
+// let floatRes = result.toFixed(2)
+
+
+// console.log(Math.floor(floatRes))
+// console.log(Math.ceil(floatRes));
+
+
+console.log(Math.random() * 6)
