768807971d077e043fd40e35c9011bf79f0a72a0
Vicky kumar <vickyjnv7@gmail.com>
2020-06-23 22:24:48 +0530

    Create coercion.js

---
+++ 02 intermediate/advanced/coercion.js
@@ -0,0 +1,24 @@
+// console.log('5' + 5);
+// const giveType = typeof {}
+
+// console.log(giveType)
+
+
+const adder = 5 + false
+console.log(adder)
+
+
+
+const loginDetials = []
+//login for getting details from db
+
+const loginId = loginDetails[0]
+
+if (loginId) {
+    console.log('allow user to login');
+
+} else {
+    console.log('auth failed')
+}
+
+//
